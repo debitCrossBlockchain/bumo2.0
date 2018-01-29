c0735bb777349bee443000099c4813dc5610d07c
pangtongqing <pangtq@163.cn>
2018-01-29 21:47:07 +0800

    update voting fee contract and doc

--- src/ledger/fees-voting.js
+++ src/ledger/fees-voting.js
@@ -1,30 +1,30 @@
 'use strict';
-const vote_records_key = 'vote_records_key';
-const enroll_records_key = 'enroll_records_key';
-const statistic_records_key = 'statistic-records_key';
-const victors_records_key = 'victors_records_key';
-const vote_records_detail_key_prefix ='vote_records_';
-let enroll_records = {};
-let vote_records = {};
-let statistic_records = [];
+const voteRecordsKey = 'voteRecordsKey';
+const enrollRecordsKey = 'enrollRecordsKey';
+const statisticRecordsKey = 'statisticRecordsKey';
+const victorsRecordsKey = 'victorsRecordsKey';
+const voteRecordsDetailKeyPrefix ='voteRecords_';
+let enrollRecords = {};
+let voteRecords = {};
+let statisticRecords = [];
 let validators = [];
 
 
-function storage_set(key, value) {
+function storageSet(key, value) {
   let result = storageStore(key,JSON.stringify(value));
   if (result === false) {
     throw 'Storage set faild';
   }
 }
 
-function storage_del(key) {
+function storageDelete(key) {
   let result = storageDel(key);
   if (result === false) {
     throw 'Storage del faild';
   }
 }
 
-function get_validators() {
+function loadValidators() {
   let result = getValidators();
   if(result === false){
     throw 'getValidators failed';
@@ -32,28 +32,28 @@ function get_validators() {
   validators = result;
 }
 
-function load_vote_records() {
-  let result = storageLoad(vote_records_key);
+function loadVoteRecords() {
+  let result = storageLoad(voteRecordsKey);
   if (result === false) {
     return false;
   } 
-  vote_records = JSON.parse(result);
+  voteRecords = JSON.parse(result);
   return true;
 }
-function load_enroll_records() {
-  let result = storageLoad(enroll_records_key);
+function loadEnrollRecords() {
+  let result = storageLoad(enrollRecordsKey);
   if (result === false) {
     return false;
   }
-  enroll_records = JSON.parse(result);
+  enrollRecords = JSON.parse(result);
   return true;
 }
 
-function enroll_exist(enroll_id) {
+function enrollExist(enrollID) {
   let exist = false;
-  Object.keys(enroll_records).every(
+  Object.keys(enrollRecords).every(
     function (key) {
-      if (enroll_records[key].enroll_id === enroll_id) {
+      if (enrollRecords[key].enrollID === enrollID) {
         exist = true;
         return false;
       } else {
@@ -64,11 +64,11 @@ function enroll_exist(enroll_id) {
   return exist;
 }
 
-function account_enrolled(enroll) {
+function accountEnrolled(enroll) {
   let enrolled = false;
-  Object.keys(enroll_records).every(
+  Object.keys(enrollRecords).every(
     function(key){
-      if(enroll_records[key].account === enroll.account){
+      if(enrollRecords[key].accountID === enroll.accountID){
         enrolled  = true;
         return false;
       }
@@ -80,80 +80,80 @@ function account_enrolled(enroll) {
   return enrolled;
 }
 
-function del_enroll(account_id, enroll_id) {
-  log('del_enroll account_id:'+ account_id +' enroll_id:'+enroll_id);
+function delEnroll(accountID, enrollID) {
+  log('delEnroll accountID:'+ accountID +' enrollID:'+enrollID);
   let be_del = false;
-  if (enroll_records[enroll_id]) {
-    if (enroll_records[enroll_id].account === account_id) {
+  if (enrollRecords[enrollID]) {
+    if (enrollRecords[enrollID].accountID === accountID) {
       be_del = true;
-      delete enroll_records[enroll_id];
-      delete vote_records[enroll_id];
-      let key=vote_records_detail_key_prefix+enroll_id;
-      storage_del(key);
-      storage_set(vote_records_key,vote_records);
-      storage_set(enroll_records_key, enroll_records);
+      delete enrollRecords[enrollID];
+      delete voteRecords[enrollID];
+      let key=voteRecordsDetailKeyPrefix+enrollID;
+      storageDelete(key);
+      storageSet(voteRecordsKey,voteRecords);
+      storageSet(enrollRecordsKey, enrollRecords);
     }
   }
   return be_del;
 }
 
-function do_voting(para) {
-  if (!(para.account && para.enroll_id && para.vote_id)) {
-    throw 'do_voting parameter error';
+function doVoting(accountID,enrollID,voteID) {
+  if (!(accountID && enrollID && voteID)) {
+    throw 'doVoting parameter error';
   }
 
-  if(load_enroll_records() === false){
+  if(loadEnrollRecords() === false){
     throw 'enroll records not exist';
   }
-  if (!enroll_exist(para.enroll_id)) {
-    throw 'Vote enroll(' + para.enroll_id + ') not exist';
+  if (!enrollExist(enrollID)) {
+    throw 'Vote enroll(' + enrollID + ') not exist';
   }
 
-  load_vote_records();
-  let key = vote_records_detail_key_prefix + para.enroll_id;
-  let vote_records_v;
-  if (vote_records[para.enroll_id]) {
+  loadVoteRecords();
+  let key = voteRecordsDetailKeyPrefix + enrollID;
+  let voteRecordBody;
+  if (voteRecords[enrollID]) {
     let result = storageLoad(key);
     if (result === false) {
-      throw 'vote_records exist key(' + para.enroll_id + ') ,but not exist body';
+      throw 'voteRecords exist key(' + enrollID + ') ,but not exist body';
     }
-    vote_records_v = JSON.parse(result);
-    if (vote_records_v[para.account]) {
-      throw 'Account(' + para.account + ') have voted the enroll(' + para.enroll_id + ')';
+    voteRecordBody = JSON.parse(result);
+    if (voteRecordBody[accountID]) {
+      throw 'Account(' + accountID + ') have voted the enroll(' + enrollID + ')';
     }
 
-    vote_records[para.enroll_id] = vote_records[para.enroll_id] + 1;
-    vote_records_v[para.account] = para;
-    storage_set(key, vote_records_v);
+    voteRecords[enrollID] = voteRecords[enrollID] + 1;
+    voteRecordBody[accountID] = {'accountID':accountID,'enrollID':enrollID,'voteID':voteID };
+    storageSet(key, voteRecordBody);
 
 
   } else {
-    vote_records[para.enroll_id] = 1;
-    vote_records_v = {};
-    vote_records_v[para.account] = para;
-    storage_set(key, vote_records_v);
+    voteRecords[enrollID] = 1;
+    voteRecordBody = {};
+    voteRecordBody[accountID] =  {'accountID':accountID,'enrollID':enrollID,'voteID':voteID };
+    storageSet(key, voteRecordBody);
   }
 
-  storage_set(vote_records_key, vote_records);
+  storageSet(voteRecordsKey, voteRecords);
   return true;
 }
 
 
-function enroll_fee(para) {
-  if(!(para.account && para.enroll_id && para.fee_type && para.price)){
-    throw 'enroll_fee parameter error';
+function enrollFee(accountID,enrollID,feeType,price) {
+  if(!(accountID && enrollID && feeType && price)){
+    throw 'enrollFee parameter error';
   }
-  load_enroll_records();
-  let exist_same_account =false;
-  Object.keys(enroll_records).every(
+  loadEnrollRecords();
+  let exist =false;
+  Object.keys(enrollRecords).every(
     function(key){
-      if(enroll_records[key].account === para.account){
-        exist_same_account =true;
-        del_enroll(enroll_records[key].account,enroll_records[key].enroll_id);
-        enroll_records[para.enroll_id] = para;
-        //vote_records[para.enroll_id]=0;
-        storage_set(enroll_records_key, enroll_records);
-        //storage_data(vote_records_key, vote_records);
+      if(enrollRecords[key].accountID === accountID){
+        exist =true;
+        delEnroll(enrollRecords[key].accountID,enrollRecords[key].enrollID);
+        enrollRecords[enrollID] = {'accountID':accountID,'enrollID':enrollID,'feeType':feeType,'price':price};
+        //voteRecords[enrollID]=0;
+        storageSet(enrollRecordsKey, enrollRecords);
+        //storage_data(voteRecordsKey, voteRecords);
         return false;
       }
       else{
@@ -162,66 +162,63 @@ function enroll_fee(para) {
     }
   );
 
-  if(!exist_same_account){
-    enroll_records[para.enroll_id] = para;
-    storage_set(enroll_records_key, enroll_records);
+  if(!exist){
+    enrollRecords[enrollID] = {'accountID':accountID,'enrollID':enrollID,'feeType':feeType,'price':price};
+    storageSet(enrollRecordsKey, enrollRecords);
   }
-  load_vote_records();   
+  loadVoteRecords();   
   return true;
 }
 
 
-function vote_statistic() {
-  get_validators();
+function voteStatistic() {
+  loadValidators();
   let thredhold = Math.floor((validators.length * 2 / 3) + 1);
-  let victors_records_tmp = [];
-  let statistic_records_tmp = [];
+  let victorsRecordsTmp = [];
+  let statisticRecordsTmp = [];
   let output = {};
 
-  Object.keys(vote_records).every(
-    function (enroll_id) {
-      let enroll = enroll_records[enroll_id];
-      if (!validators.includes(enroll_records[enroll_id].account)) { return true; }
+  Object.keys(voteRecords).every(
+    function (enrollID) {
+      let enroll = enrollRecords[enrollID];
+      if (!validators.includes(enrollRecords[enrollID].accountID)) { return true; }
 
-      if (statistic_records_tmp[enroll.fee_type]) {
-        let s = statistic_records_tmp[enroll.fee_type].statistic;
-        if (s[enroll_id]) {
-          s[enroll_id].count = vote_records[enroll_id];
-          log('thredhold:' + thredhold + ' count:' + s[enroll_id].count);
+      if (statisticRecordsTmp[enroll.feeType]) {
+        let s = statisticRecordsTmp[enroll.feeType].statistic;
+        if (s[enrollID]) {
+          s[enrollID].count = voteRecords[enrollID];
 
-          if (s[enroll_id].count >= thredhold) {
-            victors_records_tmp[enroll.fee_type] = { 'fee_type': enroll.fee_type, 'enroll_id': enroll_id, 'count': s[enroll_id].count, 'price': enroll.price };
-            output[enroll.fee_type] = enroll.price;
-            //victors_records[enroll.fee_type] =victors_records_tmp[enroll.fee_type];
+          if (s[enrollID].count >= thredhold) {
+            victorsRecordsTmp[enroll.feeType] = { 'feeType': enroll.feeType, 'enrollID': enrollID, 'count': s[enrollID].count, 'price': enroll.price };
+            output[enroll.feeType] = enroll.price;
           }
         }
       }
       else {
         let statistic = {};
-        statistic[enroll_id] = { 'enroll_id': enroll_id, 'price': enroll_records[enroll_id].price, 'count': vote_records[enroll_id] };
-        statistic_records_tmp[enroll.fee_type] = { 'fee_type': enroll.fee_type, 'statistic': statistic };
+        statistic[enrollID] = { 'enrollID': enrollID, 'price': enrollRecords[enrollID].price, 'count': voteRecords[enrollID] };
+        statisticRecordsTmp[enroll.feeType] = { 'feeType': enroll.feeType, 'statistic': statistic };
 
-        if (statistic[enroll_id].count >= thredhold) {
-          victors_records_tmp[enroll.fee_type] = { 'fee_type': enroll.fee_type, 'enroll_id': enroll_id, 'count': statistic[enroll_id].count, 'price': enroll.price };
-          output[enroll.fee_type] = enroll.price;
-          //victors_records[enroll.fee_type] =victors_records_tmp[enroll.fee_type];
+        if (statistic[enrollID].count >= thredhold) {
+          victorsRecordsTmp[enroll.feeType] = { 'feeType': enroll.feeType, 'enrollID': enrollID, 'count': statistic[enrollID].count, 'price': enroll.price };
+          output[enroll.feeType] = enroll.price;
         }
       }
       return true;
     }
   );
 
-  statistic_records = statistic_records_tmp;
-  storage_set(statistic_records_key, statistic_records);
+  statisticRecords = statisticRecordsTmp;
+  storageSet(statisticRecordsKey, statisticRecords);
 
-  if (victors_records_tmp.length > 0) {
-    storage_set(victors_records_key, victors_records_tmp);
+  if (victorsRecordsTmp.length > 0) {
+    storageSet(victorsRecordsKey, victorsRecordsTmp);
 
-    victors_records_tmp.every(
+    victorsRecordsTmp.every(
       function (item) {
-        let k = item.enroll_id;
-        if (k !== enroll_records[k].enroll_id) { throw 'enroll_records key(' + k + ') != enroll_id(' + enroll_records[k].enroll_id + ')'; }
-        del_enroll(enroll_records[k].account, enroll_records[k].enroll_id);
+        let k = item.enrollID;
+        if (k !== enrollRecords[k].enrollID) { throw 'enroll_records key(' + k + ') != enrollID(' + enrollRecords[k].enrollID + ')'; }
+        delEnroll(enrollRecords[k].accountID, enrollRecords[k].enrollID);
         return true;
       }
     );
@@ -230,36 +227,45 @@ function vote_statistic() {
   }
 }
 
-function query_voting(para) {
-  if(para.enroll_id===null || para.enroll_id===""){
-    throw 'query_voting parameter error';
+function queryVoting(enrollID) {
+  if(enrollID===null || enrollID===""){
+    throw 'queryVoting parameter error';
   }
-  load_vote_records();
-  let key =vote_records_detail_key_prefix+para.enroll_id;
+  loadVoteRecords();
+  if(voteRecords[enrollID] ===null){
+    throw 'vote_records not exist key(' + enrollID + ') vote';
+  }
+  let key =voteRecordsDetailKeyPrefix+enrollID;
   let vote_records_v={};
   let result = storageLoad(key);
   if(result === false) {
-    throw 'vote_records not exist key(' + para.enroll_id + ') vote';
+    throw 'vote_records not exist key(' + enrollID + ') vote';
   }
-  vote_records_v =JSON.parse(result);
-  return vote_records_v;
+
+  return result;
 }
 
-function query_enroll() {
-  load_enroll_records();
-  return enroll_records;
+function queryEnroll() {
+  
+  let result = storageLoad(enrollRecordsKey);
+  if (result === false) {
+    throw 'queryEnroll error';
+  }  
+  return result;
 }
 
 function main(input) {
   if (input === ''){throw 'main input is empty';}
 
   let para = JSON.parse(input);
-  if (para.do_voting) {
-    do_voting(para.do_voting);
-    vote_statistic();
+  if (para.method === 'doVoting') {
+    let p =para.params;
+    doVoting(p.accountID,p.enrollID,p.voteID);
+    voteStatistic();
   }
-  else if (para.enroll_fee) {
-    enroll_fee(para.enroll_fee);
+  else if (para.method === 'enrollFee') {
+    let p =para.params;
+    enrollFee(p.accountID,p.enrollID,p.feeType,p.price);
   }
   else {
     throw 'main input para error';
@@ -271,11 +277,12 @@ function query(input) {
   if (input === ''){ throw 'query input is empty';}
 
   let para = JSON.parse(input);
-  if (para.query_voting) {
-    return query_voting(para.query_voting);
+  if (para.method === 'queryVoting') {
+    let p =para.params;
+    return queryVoting(p.enrollID);
   }
-  else if (para.query_enroll) {
-    return query_enroll();
+  else if (para.method === 'queryEnroll') {
+    return queryEnroll();
   }
   else {
     throw 'query input para error';
