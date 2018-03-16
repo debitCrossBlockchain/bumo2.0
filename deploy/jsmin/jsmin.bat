0df6f39c57ff517dfb3492364f392ea1005983ba
dingyouzhen <dingyouzhen@outlook.com>
2018-03-16 11:33:21 +0800

    compress validators_vote.js and fees-voting.min.js

--- deploy/jsmin/jsmin.bat
+++ deploy/jsmin/jsmin.bat
@@ -1,1 +1,2 @@
-jsmin <validators_vote.js >validators_vote.min.js
+jsmin.exe <..\..\src\ledger\validators_vote.js >..\..\src\ledger\validators_vote.min.js
+jsmin.exe <..\..\src\ledger\fees-voting.js >..\..\src\ledger\fees-voting.min.js
