04b35d6ddae0a7e966097a9c223ae311eac541e5
pangtongqing <pangtq@163.cn>
2018-03-24 17:43:23 +0800

    add log for queue

--- src/glue/transaction_queue.cpp
+++ src/glue/transaction_queue.cpp
@@ -201,7 +201,12 @@ namespace bumo {
 			std::string source_address = txproto.transaction().source_address();
 			int64_t nonce = txproto.transaction().nonce();
 			std::pair<bool, TransactionFrm::pointer> result = Remove(source_address, nonce);
-			if (result.first) ++ret;
+			if (result.first)
+				++ret;
+			else
+				LOG_ERROR("RemoveTxs close_ledger_flag(%d) (%u) removed(%d) addr(%s) nonce(" FMT_I64 ") fee(" FMT_I64 ") last seq(" FMT_I64 ")",
+				(int)close_ledger, i, (int)result.first, source_address.c_str(), nonce, (int64_t)txproto.transaction().fee(), last_seq);
+
 			i++;
 			LOG_TRACE("RemoveTxs close_ledger_flag(%d) (%u) removed(%d) addr(%s) nonce(" FMT_I64 ") fee(" FMT_I64 ") last seq(" FMT_I64 ")",
 				(int)close_ledger, i, (int)result.first, source_address.c_str(), nonce, (int64_t)txproto.transaction().fee(), last_seq);
