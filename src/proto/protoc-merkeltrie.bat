227b82b1d94cdfc02271d60f6708d93622477360
riven <frm430342210@163.com>
2018-06-11 21:08:53 +0800

    Remove the golong output of protobuf

--- src/proto/protoc-merkeltrie.bat
+++ src/proto/protoc-merkeltrie.bat
@@ -1,2 +1,2 @@
-protoc.exe merkeltrie.proto --cpp_out=./cpp --java_out=./ --js_out=import_style=commonjs,binary:./js --go_out=./go
+protoc.exe merkeltrie.proto --cpp_out=./cpp --java_out=./ --js_out=import_style=commonjs,binary:./js
 pause
--- src/proto/protoc-monitor.bat
+++ src/proto/protoc-monitor.bat
@@ -1,2 +1,2 @@
-protoc.exe monitor.proto --cpp_out=./cpp --java_out=./ --js_out=import_style=commonjs,binary:./js --go_out=./go
+protoc.exe monitor.proto --cpp_out=./cpp --java_out=./ --js_out=import_style=commonjs,binary:./js
 pause
