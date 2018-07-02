d51f56d333a73513f8e05d0dfc5bd140bc3261c4
shangsony <shangsong@buaa.edu.cn>
2018-07-02 18:49:13 +0800

    Submit support for ifaddrs

--- src/utils/android_ifaddrs/ifaddrs.h
+++ src/utils/android_ifaddrs/ifaddrs.h
@@ -28,12 +28,12 @@
 
 struct ifaddrs {
 	struct ifaddrs  *ifa_next;
-	char		*ifa_name;
+	char		     *ifa_name;
 	unsigned int	 ifa_flags;
 	struct sockaddr	*ifa_addr;
 	struct sockaddr	*ifa_netmask;
 	struct sockaddr	*ifa_dstaddr;
-	void		*ifa_data;
+	void		    *ifa_data;
 };
 
 /*
