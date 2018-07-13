a6c491a80050f579a3c2aac73cd648dd89eca6c4
JianGuo.Zhu <zjg555543@163.com>
2018-07-13 18:59:55 +0800

    Modify Android bu.h

---
+++ build/android_lib/src/bu-internal.h
@@ -0,0 +1,38 @@
+#include <utils/headers.h>
+#include <common/general.h>
+#include <common/storage.h>
+#include <common/private_key.h>
+#include <common/argument.h>
+#include <common/daemon.h>
+#include <overlay/peer_manager.h>
+#include <ledger/ledger_manager.h>
+#include <consensus/consensus_manager.h>
+#include <glue/glue_manager.h>
+#include <api/web_server.h>
+#include <api/websocket_server.h>
+#include <api/console.h>
+#include <ledger/contract_manager.h>
+#include <monitor/monitor_manager.h>
+#include "configure.h"
+
+#include <time.h>
+
+void SaveWSPort();
+void RunLoop();
+
+class BuMaster : public utils::Singleton<BuMaster>, public utils::Runnable 
+{
+public:
+	BuMaster();
+	~BuMaster();
+
+	bool Initialize(const std::string &process_full_path);
+	bool Exit();
+private:
+	utils::Thread *thread_ptr_;
+	virtual void Run(utils::Thread *thread);
+	int MainLoop(int argc, char *argv[]);
+
+	std::string process_full_path_;
+};
+
