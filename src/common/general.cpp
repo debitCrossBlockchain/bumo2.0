ddac72e55655ce1b1f7655ea5a4e1ee52be5fbfb
zhaozhengyong <hardmy_1986@163.com>
2018-07-16 16:13:28 +0800

    upgrade version to 1.0.0.7

--- src/common/general.cpp
+++ src/common/general.cpp
@@ -32,7 +32,7 @@ namespace bumo {
 	const uint32_t General::LEDGER_VERSION = 1001;
 	const uint32_t General::LEDGER_MIN_VERSION = 1000;
 	const uint32_t General::MONITOR_VERSION = 1000;
-	const char *General::BUMO_VERSION = "1.0.0.6";
+	const char *General::BUMO_VERSION = "1.0.0.7";
 
 #ifdef WIN32
 	const char *General::DEFAULT_KEYVALUE_DB_PATH = "data/keyvalue.db";
