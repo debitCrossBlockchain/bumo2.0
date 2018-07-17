b5667227f1b59ab2c2595f7cf6a30196c6a238c5
JianGuo.Zhu <zjg555543@163.com>
2018-07-17 10:47:07 +0800

    Modify android build permission

--- src/3rd/curl/build_all.sh
+++ src/3rd/curl/build_all.sh
@@ -2,9 +2,9 @@
 
 for arch in armeabi-v7a 
 do
-    chmod 777 -R $BUMO_SRC_PATH/3rd/curl/
+    #chmod 777 -R $BUMO_SRC_PATH/3rd/curl/
     bash build_curl.sh $arch
-    chmod 777 -R $BUMO_SRC_PATH/3rd/curl/
+    #chmod 777 -R $BUMO_SRC_PATH/3rd/curl/
     make clean
     make -j4
     sudo make install
