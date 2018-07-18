6e93ad7c8a7ebb738475f781acc6792fe4acc25a
JianGuo.Zhu <zjg555543@163.com>
2018-07-18 10:40:23 +0800

    Add api description

--- build/android_lib/interface/bu.h
+++ build/android_lib/interface/bu.h
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 #ifndef __BU_HPP__
 #define __BU_HPP__
 
@@ -21,9 +5,30 @@
 
 #ifdef __cplusplus
 extern "C"
-#endif // __cplusplus
+#endif
 
+/********************************************************************************
+description:
+	Start the block chain program
+input: 
+	bu_home_path:    The buchain path. For example, "/sdcard/buchain/"
+output:
+	
+return:
+	0：OK，other error
+********************************************************************************/
 int Init(char *bu_home_path);
+
+/********************************************************************************
+description:
+	Stop the block chain program
+input: 
+	bu_home_path:    The buchain path. For example, "/sdcard/buchain/"
+output:
+	
+return:
+	0：OK，other error
+********************************************************************************/
 int UnInit();
 
-#endif //__BU_HPP__
+#endif
