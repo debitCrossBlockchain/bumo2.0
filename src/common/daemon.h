0e8c6e15889cbdb5cca47317267057f5967556fa
wilsonyou <40747875+wilsonyou@users.noreply.github.com>
2018-07-26 10:01:38 +0800

    Update daemon.h

--- src/common/daemon.h
+++ src/common/daemon.h
@@ -55,7 +55,7 @@ namespace utils {
 		void GetModuleStatus(Json::Value &data) {
 
 		};
-		//virtual void GetModuleStatus(Json::Value &data) const;
+		//Virtual void GetModuleStatus(Json::Value &data) const;
 	};
 }
 #endif
