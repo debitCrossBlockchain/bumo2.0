2848c2b55f0d7efd8ed4a320dea2a7f91157ac88
shangsong <shangsong@bubi.cn>
2018-11-05 15:20:28 +0800

    update event interface ，eg OnCrateChildChain、OnMainChainMix etc

--- src/cross/message_channel_manager.cpp
+++ src/cross/message_channel_manager.cpp
@@ -1,11 +1,141 @@
 #include "message_channel_manager.h"
 
+namespace bumo {
 
-MessageChannelManager::MessageChannelManager()
-{
-}
+	MessageChannelManager::MessageChannelManager(server *server_h, client *client_h, tls_server *tls_server_h, tls_client *tls_client_h, connection_hdl con, const std::string &uri, int64_t id) :
+		Connection(server_h, client_h, tls_server_h, tls_client_h, con, uri, id) {
+	}
 
 
-MessageChannelManager::~MessageChannelManager()
-{
+	MessageChannelManager::~MessageChannelManager(){
+	}
+
+
+
+	MessageChannelServer::MessageChannelServer() : Network(SslParameter()) {
+		connect_interval_ = 120 * utils::MICRO_UNITS_PER_SEC;
+		last_connect_time_ = 0;
+
+		/*request_methods_[protocol::MESSAGE_CHANNEL_CREATE_CHILD_CHAIN] = std::bind(&MessageChannelServer::OnCrateChildChain, this, std::placeholders::_1, std::placeholders::_2);
+		request_methods_[protocol::MESSAGE_CHANNEL_MAIN_MIX] = std::bind(&MessageChannelServer::OnMainChainMix, this, std::placeholders::_1, std::placeholders::_2);
+		request_methods_[protocol::CHAIN_SUBMITTRANSACTION] = std::bind(&MessageChannelServer::OnSubmitTransaction, this, std::placeholders::_1, std::placeholders::_2);
+		request_methods_[protocol::CHAIN_SUBSCRIBE_TX] = std::bind(&MessageChannelServer::OnSubscribeTx, this, std::placeholders::_1, std::placeholders::_2);*/
+		thread_ptr_ = NULL;
+	}
+
+	MessageChannelServer::~MessageChannelServer() {
+		if (thread_ptr_){
+			delete thread_ptr_;
+		}
+	}
+
+	bool MessageChannelServer::Initialize(MessageChannelConfigure & message_channel_configure) {
+		thread_ptr_ = new utils::Thread(this);
+		if (!thread_ptr_->Start("messageChannel")) {
+			return false;
+		}
+
+		StatusModule::RegisterModule(this);
+		LOG_INFO("Initialized message channel server successfully");
+		return true;
+	}
+
+	bool MessageChannelServer::Exit() {
+		Stop();
+		thread_ptr_->JoinWithStop();
+		return true;
+	}
+
+	void MessageChannelServer::Run(utils::Thread *thread) {
+		Start(bumo::Configure::Instance().message_channel_configure_.listen_address_);
+	}
+
+
+	bool MessageChannelServer::OnCrateChildChain(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnMainChainMix(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnChildChainMix(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+	bool MessageChannelServer::OnDeposit(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnWithdrawal(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnFastWithdrawal(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnSubmitChildHead(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnChallengeWithdrawal(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnChallengeChildHead(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnChildChainGeneses(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnSubmitTransaction(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	bool MessageChannelServer::OnSubscribeTx(protocol::MessageChannel &message, int64_t conn_id){
+		return true;
+	}
+
+	void MessageChannelServer::BroadcastMsg(int64_t type, const std::string &data) {
+		utils::MutexGuard guard(conns_list_lock_);
+
+		for (ConnectionMap::iterator iter = connections_.begin();
+			iter != connections_.end();
+			iter++) {
+			std::error_code ec;
+			iter->second->SendRequest(type, data, ec);
+		}
+	}
+
+	void MessageChannelServer::BroadcastChainTxMsg(const protocol::TransactionEnvStore& tx_msg) {
+		utils::MutexGuard guard(conns_list_lock_);
+
+	/*	for (auto iter = connections_.begin(); iter != connections_.end(); iter++) {
+			WsPeer *peer = (WsPeer *)iter->second;
+			if (peer->Filter(tx_msg)) {
+				std::error_code ec;
+				std::string str = tx_msg.SerializeAsString();
+				peer->SendRequest(protocol::CHAIN_TX_ENV_STORE, str, ec);
+			}
+		}*/
+	}
+
+	void MessageChannelServer::GetModuleStatus(Json::Value &data) {
+		data["name"] = "message_channel_server";
+		data["listen_port"] = GetListenPort();
+		Json::Value &peers = data["clients"];
+		int32_t active_size = 0;
+		utils::MutexGuard guard(conns_list_lock_);
+		for (auto &item : connections_) {
+			item.second->ToJson(peers[peers.size()]);
+		}
+	}
+
+	Connection *MessageChannelServer::CreateConnectObject(server *server_h, client *client_,
+		tls_server *tls_server_h, tls_client *tls_client_h,
+		connection_hdl con, const std::string &uri, int64_t id) {
+		return new MessageChannelManager(server_h, client_, tls_server_h, tls_client_h, con, uri, id);
+	}
 }
--- src/cross/message_channel_manager.h
+++ src/cross/message_channel_manager.h
@@ -18,13 +18,14 @@ along with bumo.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <proto/cpp/chain.pb.h>
 #include <proto/cpp/overlay.pb.h>
+#include<main/configure.h>
 #include <common/network.h>
 #include <monitor/system_manager.h>
 
 namespace bumo {
 	class MessageChannelManager :public Connection{
 	public:
-		MessageChannelManager();
+		MessageChannelManager(server *server_h, client *client_h, tls_server *tls_server_h, tls_client *tls_client_h, connection_hdl con, const std::string &uri, int64_t id);
 		~MessageChannelManager();
 	};
 
@@ -40,12 +41,20 @@ namespace bumo {
 
 		//virtual bool Send(const ZMQTaskType type, const std::string& buf);
 
-		bool Initialize(WsServerConfigure & ws_server_configure);
+		bool Initialize(MessageChannelConfigure & message_channel_configure);
 		bool Exit();
 
 		// Handlers
-		bool OnChainHello(protocol::MessageChannel &message, int64_t conn_id);
-		bool OnChainPeerMessage(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnCrateChildChain(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnMainChainMix(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnChildChainMix(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnDeposit(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnWithdrawal(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnFastWithdrawal(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnSubmitChildHead(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnChallengeWithdrawal(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnChallengeChildHead(protocol::MessageChannel &message, int64_t conn_id);
+		bool OnChildChainGeneses(protocol::MessageChannel &message, int64_t conn_id);
 		bool OnSubmitTransaction(protocol::MessageChannel &message, int64_t conn_id);
 		bool OnSubscribeTx(protocol::MessageChannel &message, int64_t conn_id);
 
