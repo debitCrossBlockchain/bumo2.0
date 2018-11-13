096854d40ea3301329c72f034ea6488dda29e474
xianguangtuan <463517114@qq.com>
2018-11-13 15:35:01 +0800

    modify grammar format

--- src/cross/block_listen_manager.cpp
+++ src/cross/block_listen_manager.cpp
@@ -23,17 +23,12 @@ namespace bumo {
 
 		return true;
 	}
-	/*
-	bool BlockListenManager::CommitBlock(){
-		HandleMainChainBlock();
-		HandleChildChainBlock();
-		return true;
-	}
-	*/
-	bool BlockListenManager::HandleBlock(LedgerFrm::pointer closing_ledger){
-		HandleMainChainBlock(closing_ledger);
-		HandleChildChainBlock(closing_ledger);
-		return true;
+
+	void BlockListenManager::HandleBlock(LedgerFrm::pointer closing_ledger){
+		if (General::GetSelfChainId() == General::MAIN_CHAIN_ID)
+			HandleMainChainBlock(closing_ledger);
+		else
+			HandleChildChainBlock(closing_ledger);
 	}
 
 	protocol::MESSAGE_CHANNEL_TYPE BlockListenManager::FilterTlog(std::string tlog_topic)
@@ -87,7 +82,6 @@ namespace bumo {
 							LOG_ERROR("tlog parames number should have 5,but now is ", log.datas_size());
 							return nullptr;
 						}
-						int iSize = log.datas_size();
 						LOG_INFO("get tlog topic:%s,args[0]:%s,args[1]:%s,args[2]:%s", log.topic().c_str(), log.datas(0).c_str(), log.datas(1).c_str(), log.datas(2).c_str());
 						return &log;
 					}
@@ -106,39 +100,41 @@ namespace bumo {
 			TransactionFrm::pointer tx = closing_ledger->apply_tx_frms_[i];
 			//const protocol::Transaction &tran = ledger.transaction_envs(i).transaction();
 			//
-			const protocol::OperationLog *pTlog = PickTransferTlog(tx);
-			if (nullptr != pTlog)
+			const protocol::OperationLog *tlog = PickTransferTlog(tx);
+			if (nullptr != tlog)
 			{
 				protocol::MessageChannel msg_channel;
-				const std::string &strTlogParams = pTlog->datas(4);
+				const std::string &tlog_params = tlog->datas(4);
 				//tlog param(0)
-				if (strTlogParams.size() == 0 || strTlogParams.size() > General::TRANSACTION_LOG_DATA_MAXSIZE)
+				if (tlog_params.size() == 0 || tlog_params.size() > General::TRANSACTION_LOG_DATA_MAXSIZE)
 				{
 					LOG_ERROR("Log's parameter data size should be between (0,%d]", General::TRANSACTION_LOG_DATA_MAXSIZE);
 					break;
 				}
 				//LOG_INFO("get tlog topic:%s,args[0]:%s", log.topic(), log.datas(j));
 				Json::Value trans_json;
-				if (!trans_json.fromString(strTlogParams)) {
+				if (!trans_json.fromString(tlog_params)) {
 					LOG_ERROR("Failed to parse the json content of the tlog");
 					continue;
 				}
-				msg_channel.set_target_chain_id(atoi(pTlog->datas(3).c_str()));
+				msg_channel.set_target_chain_id(atoi(tlog->datas(3).c_str()));
 
-				protocol::MESSAGE_CHANNEL_TYPE msg_type = FilterTlog(pTlog->topic());
+				protocol::MESSAGE_CHANNEL_TYPE msg_type = FilterTlog(tlog->topic());
 				msg_channel.set_msg_type(msg_type);
 
-				::google::protobuf::Message * pMsg = GetMsgObject(msg_type);
+				::google::protobuf::Message * msg = GetMsgObject(msg_type);
+				if (!msg)
+					continue;
 				std::string error_msg;
-				if (!Json2Proto(trans_json, *pMsg, error_msg)) {
+				if (!Json2Proto(trans_json, *msg, error_msg)) {
 					LOG_ERROR("Failed to Json2Proto error_msg=%s",error_msg.c_str());
 					continue;
 				}
-				msg_channel.set_msg_data(pMsg->SerializeAsString().c_str());
+				msg_channel.set_msg_data(msg->SerializeAsString().c_str());
 
 				MessageChannel::GetInstance()->MessageChannelProducer(msg_channel);
 
-				delete pMsg;
+				delete msg;
 
 			}
 		}
@@ -147,18 +143,17 @@ namespace bumo {
 
 	void BlockListenManager::HandleChildChainBlock(LedgerFrm::pointer closing_ledger){
 		//TODO: Handel child chain block, and call MessageChannel to send main chain proc 
-		if (General::GetSelfChainId() != General::MAIN_CHAIN_ID)
-		{
-			//send to messagechanel
-			protocol::LedgerHeader& ledger_header = closing_ledger->GetProtoHeader();
-			protocol::MessageChannel msg_channel;
-			//sendto main chain id=0
-			msg_channel.set_target_chain_id(General::MAIN_CHAIN_ID);
-			msg_channel.set_msg_type(protocol::MESSAGE_CHANNEL_TYPE::MESSAGE_CHANNEL_SUBMIT_HEAD);
-			msg_channel.set_msg_data(ledger_header.SerializeAsString().c_str());
 
-			MessageChannel::GetInstance()->MessageChannelProducer(msg_channel);
-			LOG_INFO("childChain build a block hash=%s,send msgchannel", utils::String::BinToHexString(ledger_header.hash()).c_str());
-		}
+		//send to messagechanel
+		protocol::LedgerHeader& ledger_header = closing_ledger->GetProtoHeader();
+		protocol::MessageChannel msg_channel;
+		//sendto main chain id=0
+		msg_channel.set_target_chain_id(General::MAIN_CHAIN_ID);
+		msg_channel.set_msg_type(protocol::MESSAGE_CHANNEL_TYPE::MESSAGE_CHANNEL_SUBMIT_HEAD);
+		msg_channel.set_msg_data(ledger_header.SerializeAsString().c_str());
+
+		MessageChannel::GetInstance()->MessageChannelProducer(msg_channel);
+		LOG_INFO("childChain build a block hash=%s,send msgchannel", utils::String::BinToHexString(ledger_header.hash()).c_str());
+
 	}
 }
