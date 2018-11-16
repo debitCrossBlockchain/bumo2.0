7cb6f4c435bd5e2f1d552522b65fc9f18b2723dd
xianguangtuan <463517114@qq.com>
2018-11-16 14:19:32 +0800

    optimize: reduce code circle

--- src/cross/block_listen_manager.cpp
+++ src/cross/block_listen_manager.cpp
@@ -65,24 +65,25 @@ namespace bumo {
 				continue;
 
 			for (int j = 0; j < trans.operations_size(); j++){
-				if (protocol::Operation_Type_LOG == trans.operations(j).type()){
-					const protocol::OperationLog &log = trans.operations(j).log();
-					if (log.topic().size() == 0 || log.topic().size() > General::TRANSACTION_LOG_TOPIC_MAXSIZE){
-						LOG_ERROR("Log's parameter topic size should be between (0,%d]", General::TRANSACTION_LOG_TOPIC_MAXSIZE);
-						continue;
-					}
-					//special transaction
-					if (FilterTlog(log.topic()) == protocol::MESSAGE_CHANNEL_TYPE::MESSAGE_CHANNEL_TYPE_NONE){
-						continue;
-					}
-					//transfer tlog params must be 2
-					if (log.datas_size() != 2){
-						LOG_ERROR("tlog parames number should have 2,but now is ", log.datas_size());
-						return nullptr;
-					}
-					LOG_INFO("get tlog topic:%s,args[0]:%s,args[1]:%s", log.topic().c_str(), log.datas(0).c_str(), log.datas(1).c_str());
-					return &log;
+
+				if (protocol::Operation_Type_LOG != trans.operations(j).type())
+					continue;
+				const protocol::OperationLog &log = trans.operations(j).log();
+				if (log.topic().size() == 0 || log.topic().size() > General::TRANSACTION_LOG_TOPIC_MAXSIZE){
+					LOG_ERROR("Log's parameter topic size should be between (0,%d]", General::TRANSACTION_LOG_TOPIC_MAXSIZE);
+					continue;
 				}
+				//special transaction
+				if (FilterTlog(log.topic()) == protocol::MESSAGE_CHANNEL_TYPE::MESSAGE_CHANNEL_TYPE_NONE){
+					continue;
+				}
+				//transfer tlog params must be 2
+				if (log.datas_size() != 2){
+					LOG_ERROR("tlog parames number should have 2,but now is ", log.datas_size());
+					return nullptr;
+				}
+				LOG_INFO("get tlog topic:%s,args[0]:%s,args[1]:%s", log.topic().c_str(), log.datas(0).c_str(), log.datas(1).c_str());
+				return &log;
 			}
 		}
 		return nullptr;
@@ -97,37 +98,38 @@ namespace bumo {
 			//const protocol::Transaction &tran = ledger.transaction_envs(i).transaction();
 			//
 			const protocol::OperationLog *tlog = PickTransferTlog(tx);
-			if (nullptr != tlog){
-				protocol::MessageChannel msg_channel;
-				const std::string &tlog_params = tlog->datas(1);
-				//tlog param(0)
-				if (tlog_params.size() == 0 || tlog_params.size() > General::TRANSACTION_LOG_DATA_MAXSIZE){
-					LOG_ERROR("Log's parameter data size should be between (0,%d]", General::TRANSACTION_LOG_DATA_MAXSIZE);
-					break;
-				}
-				//LOG_INFO("get tlog topic:%s,args[0]:%s", log.topic(), log.datas(j));
-				Json::Value trans_json;
-				if (!trans_json.fromString(tlog_params)) {
-					LOG_ERROR("Failed to parse the json content of the tlog");
-					continue;
-				}
-				msg_channel.set_target_chain_id(atoi(tlog->datas(0).c_str()));
-
-				protocol::MESSAGE_CHANNEL_TYPE msg_type = FilterTlog(tlog->topic());
-				msg_channel.set_msg_type(msg_type);
-
-				std::shared_ptr<Message> msg = GetMsgObject(msg_type);
-				if (!msg)
-					continue;
-				std::string error_msg;
-				if (!Json2Proto(trans_json, *msg, error_msg)) {
-					LOG_ERROR("Failed to Json2Proto error_msg=%s",error_msg.c_str());
-					continue;
-				}
-				msg_channel.set_msg_data(msg->SerializeAsString());
-
-				MessageChannel::GetInstance()->MessageChannelProducer(msg_channel);
+			if (nullptr == tlog)
+				continue;
+			protocol::MessageChannel msg_channel;
+			const std::string &tlog_params = tlog->datas(1);
+			//tlog param(0)
+			if (tlog_params.size() == 0 || tlog_params.size() > General::TRANSACTION_LOG_DATA_MAXSIZE){
+				LOG_ERROR("Log's parameter data size should be between (0,%d]", General::TRANSACTION_LOG_DATA_MAXSIZE);
+				break;
 			}
+			//LOG_INFO("get tlog topic:%s,args[0]:%s", log.topic(), log.datas(j));
+			Json::Value trans_json;
+			if (!trans_json.fromString(tlog_params)) {
+				LOG_ERROR("Failed to parse the json content of the tlog");
+				continue;
+			}
+			msg_channel.set_target_chain_id(atoi(tlog->datas(0).c_str()));
+
+			protocol::MESSAGE_CHANNEL_TYPE msg_type = FilterTlog(tlog->topic());
+			msg_channel.set_msg_type(msg_type);
+
+			std::shared_ptr<Message> msg = GetMsgObject(msg_type);
+			if (!msg)
+				continue;
+			std::string error_msg;
+			if (!Json2Proto(trans_json, *msg, error_msg)) {
+				LOG_ERROR("Failed to Json2Proto error_msg=%s", error_msg.c_str());
+				continue;
+			}
+			msg_channel.set_msg_data(msg->SerializeAsString());
+
+			MessageChannel::GetInstance()->MessageChannelProducer(msg_channel);
+
 		}
 	}
 
