a20e76cc63ea3edfe27d1e25e75d1df305f50a20
zhujianguo <zjg555543@163.com>
2018-11-30 17:59:08 +0800

    Add Transaction sender

--- src/cross/cross_utils.cpp
+++ src/cross/cross_utils.cpp
@@ -1,24 +1,25 @@
 /*
-bumo is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+	bumo is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-bumo is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+	bumo is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with bumo.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with bumo.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <glue/glue_manager.h>
 #include <overlay/peer_manager.h>
 #include<ledger/fee_calculate.h>
 #include <utils/base_int.h>
-
 #include "cross_utils.h"
+
+#define MAX_SEND_TRANSACTION_TIMES 50
 
 namespace bumo {
 
@@ -137,5 +138,133 @@ namespace bumo {
 
 		TransactionFrm::pointer ptr = std::make_shared<TransactionFrm>(tran_env);
 		return ptr;
+	}
+
+	TransactionSender::TransactionSender(){
+		enabled_ = false;
+		thread_ptr_ = NULL;
+		cur_nonce_ = 0;
+		last_update_time_ = utils::Timestamp::HighResolution();
+	}
+	TransactionSender::~TransactionSender(){
+		if (thread_ptr_){
+			delete thread_ptr_;
+			thread_ptr_ = NULL;
+		}
+	}
+
+	bool TransactionSender::Initialize(const std::string &private_key){
+		enabled_ = true;
+		thread_ptr_ = new utils::Thread(this);
+		if (!thread_ptr_->Start("ProposerManager")) {
+			return false;
+		}
+
+		PrivateKey pkey(private_key);
+		if (!pkey.IsValid()){
+			LOG_ERROR("Private key is not valid");
+			return false;
+		}
+
+		private_key_ = private_key;
+		source_address_ = pkey.GetEncAddress();
+		return true;
+	}
+
+	bool TransactionSender::Exit(){
+		enabled_ = false;
+		if (thread_ptr_) {
+			thread_ptr_->JoinWithStop();
+		}
+		return true;
+	}
+
+	void TransactionSender::SendTransaction(ITransactionSenderNotify *notify, const TransTask &trans_task){
+		assert(trans_task.amount_ >= 0);
+		assert(!trans_task.dest_address_.empty());
+		assert(!trans_task.input_paras_.empty());
+		assert(notify != nullptr);
+		
+		utils::MutexGuard guard(task_vector_lock_);
+		TransTaskVector &task_vector = trans_task_map_[notify];
+		task_vector.push_back(trans_task);
+	}
+	void TransactionSender::Run(utils::Thread *thread){
+		while (enabled_){
+			int64_t current_time = utils::Timestamp::HighResolution();
+			if ((current_time - last_update_time_) <= 5 * utils::MICRO_UNITS_PER_SEC){
+				continue;
+			}
+
+			SendingAll();
+			last_update_time_ = current_time;
+		}
+	}
+
+	void TransactionSender::SendingAll(){
+		TransTaskMap temp_map;
+		do
+		{
+			utils::MutexGuard guard(task_vector_lock_);
+			temp_map.swap(trans_task_map_);
+			trans_task_map_.clear();
+			assert(trans_task_map_.size() == 0);
+		} while (false);
+		if (temp_map.empty()){
+			return;
+		}
+
+		AccountFrm::pointer account_ptr;
+		if (!Environment::AccountFromDB(source_address_, account_ptr)) {
+			LOG_ERROR("Address:%s not exsit", source_address_.c_str());
+			return;
+		}
+		cur_nonce_ = account_ptr->GetAccountNonce() + 1;
+		
+		for (TransTaskMap::const_iterator itr = temp_map.begin(); itr != temp_map.end(); itr++){
+			ITransactionSenderNotify* notify = itr->first;
+			const TransTaskVector &task_vector = itr->second;
+			for (uint32_t i = 0; i < task_vector.size(); i++){
+				const TransTask &trans_task = task_vector[i];
+				TransTaskResult task_result = SendingSingle(trans_task.input_paras_);
+				notify->HandleTransactionSenderResult(trans_task, task_result);
+			}
+		}
+	}
+
+	TransTaskResult TransactionSender::SendingSingle(const std::vector<std::string> &paras){
+		int32_t err_code = 0;
+
+		for (int i = 0; i <= MAX_SEND_TRANSACTION_TIMES; i++){
+			TransactionFrm::pointer trans = CrossUtils::BuildTransaction(private_key_, General::CONTRACT_CMC_ADDRESS, paras, cur_nonce_);
+			if (nullptr == trans){
+				LOG_ERROR("Trans pointer is null");
+				continue;
+			}
+			std::string hash = utils::String::BinToHexString(trans->GetContentHash().c_str());
+			err_code = CrossUtils::SendTransaction(trans);
+			switch (err_code)
+			{
+				case protocol::ERRCODE_SUCCESS:
+				case protocol::ERRCODE_ALREADY_EXIST:{
+					cur_nonce_++;
+					TransTaskResult task_result(true, "", hash);
+					return task_result;
+				}
+				case protocol::ERRCODE_BAD_SEQUENCE:{
+					cur_nonce_++;
+					continue;
+				}
+				default:{
+					LOG_ERROR("Send transaction erro code:%d", err_code);
+					continue;
+				}
+			}
+
+			utils::Sleep(10);
+		}
+
+		TransTaskResult task_result(false, "Try MAX_SEND_TRANSACTION_TIMES times", "");
+		return task_result;
 	}
 }
