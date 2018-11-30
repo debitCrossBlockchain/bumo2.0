d03e6979fc3da822c81119c9e2ee81599591cbce
zhujianguo <zjg555543@163.com>
2018-11-30 17:59:47 +0800

    Modify proposer_manager.h

--- src/cross/proposer_manager.h
+++ src/cross/proposer_manager.h
@@ -18,17 +18,19 @@ along with bumo.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <utils/singleton.h>
 #include <utils/thread.h>
+#include <cross/cross_utils.h>
 #include<cross/message_channel_manager.h>
 
 #define MAX_CHAIN_ID 1000
-#define MAX_SEND_TRANSACTION_TIMES 10
-#define MAX_REQUEST_BLOCK_NUMS 10
+#define MAX_REQUEST_BLOCK_NUMS 100
 #define MAX_ERROR_TX_COUNT 5
+#define MAX_PACK_TX_COUNT 5
+#define PROPOSER_PERIOD 15
 
 namespace bumo {
 
 	class ProposerManager :public utils::Singleton<ProposerManager>,
-		public bumo::IMessageChannelConsumer,
+		public bumo::IMessageChannelConsumer, public ITransactionSenderNotify,
 		public utils::Runnable{
 		friend class utils::Singleton<bumo::ProposerManager>;
 
@@ -40,7 +42,7 @@ namespace bumo {
 			int64_t recv_max_seq;
 			int64_t cmc_latest_seq;
 			int64_t error_tx_times = 0;
-			utils::StringList   error_info_list;
+			utils::StringList tx_history;
 			utils::StringVector cmc_latest_validates;
 		public:
 			void Reset() {
@@ -58,11 +60,12 @@ namespace bumo {
 
 		bool Initialize();
 		bool Exit();
-		void UpdateTransactionErrorInfo(const int64_t &error_code,const std::string &error_desc,const std::string& hash);
+		void UpdateTransactionResult(const int64_t &error_code,const std::string &error_desc,const std::string& hash);
 
 	private:
 		virtual void Run(utils::Thread *thread) override;
 		virtual void HandleMessageChannelConsumer(const protocol::MessageChannel &message_channel) override;
+		virtual void HandleTransactionSenderResult(const TransTask &task_task, const TransTaskResult &task_result) override;
 
 		void UpdateLatestStatus();
 		void UpdateLatestValidates(const int64_t chain_id, utils::StringVector &latest_validates);
@@ -70,7 +73,6 @@ namespace bumo {
 		void SortChildSeq(ChildChain &child_chain);
 		void RequestChainSeq(int64_t chain_id, int64_t seq);
 		void ProposeBlocks();
-		void SendTransaction(const std::vector<std::string> &paras,std::string& hash);
 		void BreakProposer(const std::string &error_des);
 
 	private:
@@ -80,9 +82,7 @@ namespace bumo {
 		utils::Mutex child_chain_map_lock_;
 		ChildChain child_chain_maps_[MAX_CHAIN_ID];
 
-		int64_t last_update_time_;
 		int64_t last_propose_time_;
-		int64_t cur_nonce_;
 		bool main_chain_;
 		std::string source_address_;
 	};
