0af38152090145663715c36d5a52b9c4b80f6b4d
shangsong <shangsong@buaa.edu.cn>
2018-12-18 17:30:53 +0800

    delete include

--- src/cross/cross_manager.cpp
+++ src/cross/cross_manager.cpp
@@ -1,5 +1,4 @@
 #include <cross/cross_manager.h>
-#include<cross/message_channel_manager.h>
 #include<cross/block_listen_manager.h>
 #include<cross/main_proposer_manager.h>
 #include<cross/child_proposer_manager.h>
