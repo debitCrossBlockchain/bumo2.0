eef17591786a6ad4ade62562d3c80516b2f32174
shangsong <shangsong@buaa.edu.cn>
2018-12-18 16:45:16 +0800

    add quote

--- src/cross/cross_manager.cpp
+++ src/cross/cross_manager.cpp
@@ -1,4 +1,9 @@
-#include "cross_manager.h"
+#include <cross/cross_manager.h>
+#include<cross/message_channel_manager.h>
+#include<cross/block_listen_manager.h>
+#include<cross/main_proposer_manager.h>
+#include<cross/child_proposer_manager.h>
+#include<cross/challenge_manager.h>
 namespace bumo {
 
 	CrossManager::CrossManager(){
